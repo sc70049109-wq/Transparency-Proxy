c3e955655971d560b14d33d133ac3b07d0cddb30
sc70049109-wq <sc70049109@gmail.com>
2025-11-17 17:36:47 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,9 +1,3 @@
-// Dark / light mode toggle
-const themeBtn = document.getElementById("theme-toggle");
-themeBtn?.addEventListener("click", () => {
-    if(document.body.style.filter === "brightness(1.2)") {
-        document.body.style.filter = "brightness(1)";
-    } else {
-        document.body.style.filter = "brightness(1.2)";
-    }
+document.getElementById("enter-btn").addEventListener("click", () => {
+    window.location.href = "/proxy.html";
 });
